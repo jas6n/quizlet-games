b0e4693c4dcbe41d6ee28d4bb8bf156b66f56020
jas6n <73123996+jas6n@users.noreply.github.com>
2024-08-27 14:17:09 -0400

    movement changed

--- game.py
+++ game.py
@@ -3,9 +3,9 @@ import os
 import random
       
 
-terms = ["Lansing", "Albany", "Springfield", "Indianapolis", "Columbus", "Sacramento"]
+terms = ["Lansing", "Albany" , "Springfield", "Indianapolis"]#, "Columbus", "Sacramento"]
 
-definitions = ["Capital of Michigan", "Capital of New York", "Capital of Illinois", "Capital of Indiana", "Capital of Ohio", "Capital of California"]
+definitions = ["Capital of Michigan", "Capital of New York", "Capital of Illinois", "Capital of Indiana"]#, "Capital of Ohio", "Capital of California"]
 
 pygame.init()
 
@@ -22,18 +22,46 @@ class Player:
         self.y = y
         self.IMGS = BIRD_IMGS
         self.img = self.IMGS[0]
+        self.moving_up = False
+        self.moving_down = False
+        self.move_counter = 0
+        self.running_position = 1
 
-    def move(self, space_pressed, min, max): # make it so players move with a b c d or 1 2 3 4 or move up to a certain area so you dont have to spam space bar
 
-        if space_pressed:
-            self.y -= 4
+    def move(self, up, down, height): # make it so players move with a b c d or 1 2 3 4 or move up to a certain area so you dont have to spam space bar
+
+        # range = max - min
+
+        # 4 points it should be able to go: 1 * (height - 100)/ 4 - 25, ...
+
+
+        if self.moving_up:         
+            if self.move_counter < 20:
+                self.y -= (height - 100)/ 80
+                self.move_counter += 1
+            else: 
+                self.move_counter = 0
+                self.moving_up = False
+        elif self.moving_down:
+
+            if self.move_counter < 20:
+                self.y += (height - 100)/ 80
+                self.move_counter += 1
+            else: 
+                self.move_counter = 0
+                self.moving_down = False
         else:
-            self.y += 3
+            if up:
+                if self.running_position != 1:
+                    self.moving_up = True
+                    self.running_position -= 1
+            if down:
+                if self.running_position != 4:
+                    self.moving_down = True
+                    self.running_position += 1
 
-        if self.y < min:
-            self.y = min
-        elif self.y > max:
-            self.y = max
+
+
 
 
     def draw(self, screen):
@@ -56,6 +84,7 @@ class Obstacle:
     def change_pos(self, x, y):
         self.x = x
         self.y = y
+
 
     def change_white(self):
         self.color = (255, 255, 255)
@@ -85,7 +114,7 @@ class Obstacle:
 class Game:
 
     def __init__(self, width, height):
-        self.player = Player(250, 50)
+        self.player = Player(250, (height - 100)/ 4 - 25) # 4 * (height - 100)/ 4 - 25
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
@@ -97,6 +126,7 @@ class Game:
         self.terms = []
         self.definitions = []
         self.indices = []
+        self.added = False
         self.a = 0
         self.b = 0
         self.c = 0
@@ -105,6 +135,7 @@ class Game:
         self.current = []
         self.cutoff = 0
         self.learn_freq = 3
+        self.correct = 0
         self.i = 0
         self.wave = True
         self.mode = True # true means terms are being showed as the answer choices
@@ -116,30 +147,25 @@ class Game:
             if (len(self.current) == 0): # if current block is empty
                 for i in range(self.learn_freq):
                     if i + self.cutoff < len(self.terms):
-                        self.current.append(i + self.cutoff) # add the current working indices
+                        self.current.append(self.indices[i + self.cutoff]) # add the current working indices
                     else:
+
                         break
 
                 self.cutoff += self.learn_freq
 
-                if self.cutoff >= len(self.terms):
-                    self.reviewed_all = True
-                    self.current = self.indices
-                    self.i = 0
+            if self.i == len(self.indices) - 1:
+                self.reviewed_all = True
+                self.added = True
 
-            temp = self.i
-            self.i += 1 
-            self.i = self.i % self.learn_freq
+        return self.current[0] # return the current problem
+        # else:
 
-            return self.current[temp] # return the current problem
-        else:
-
-            temp = self.i
-            self.i += 1 
+        #     temp = self.i
+        #     self.i += 1 
 
 
-            return self.current[temp] # return the reviewed problem
-
+        #     return self.current[temp] # return the reviewed problem
 
 
     def draw_screen(self): # background of screen
@@ -155,6 +181,7 @@ class Game:
             if self.lines[i] < 0:
                 self.lines[i] = self.width
 
+
     def terms_and_defs(self):
 
         for i in range(len(terms)): # initialize terms and defs arrays in game class
@@ -164,20 +191,59 @@ class Game:
 
         random.shuffle(self.indices) # shuffle the cards
 
-    def randomize_choices(self, a=False, b=False, c=False, d=False):
 
-        sample = random.sample(range(0, len(terms)), 4)
+    def detect_collision(self):
+
+        if self.player.x >= self.terms[0].x:
+            if self.player.running_position == 1:
+                return 1
+            elif self.player.running_position == 2:
+                return 2
+            elif self.player.running_position == 3:
+                return 3
+            elif self.player.running_position == 4:
+                return 4
+            
+        else:
+            return 0
 
 
-        if not a:
-            self.a = sample[0]
-        if not b:
-            self.b = sample[1]
-        if not c:
-            self.c = sample[2]
-        if not d:
-            self.d = sample[3]
 
+    def randomize_choices(self, element):
+
+        temp = self.indices[-1]
+        # Swap the excluded element with the last element
+        self.indices[-1], self.indices[self.i] = self.indices[self.i], temp
+        
+        # Sample from the array, excluding the last element (which now holds the excluded number)
+        result = random.sample(self.indices[:-1], 3)
+        
+        # Optional: Swap back to maintain the original array order
+        temp = self.indices[-1]
+        self.indices[-1], self.indices[self.i] = self.indices[self.i], temp
+
+        result.append(element)
+
+        random.shuffle(result)
+
+        self.a = result[0]
+        self.b = result[1]
+        self.c = result[2]
+        self.d = result[3]
+                
+        self.i += 1
+
+        self.current.pop(0)
+
+        # return where the correct answer isg
+        if self.a == element:
+            return 1
+        if self.b == element:
+            return 2
+        if self.c == element:
+            return 3
+        if self.d == element:
+            return 4
 
 
     def move_obstacles(self):
@@ -185,7 +251,12 @@ class Game:
 
         if self.wave:
 
-            self.randomize_choices()
+            self.correct = self.randomize_choices(self.learn())
+            if self.added:
+                for i in self.indices:
+                    self.current.append(i)
+                self.i = 0
+                self.added = False
             self.terms[self.a].change_black()
             self.terms[self.a].change_pos(self.width, 50)
 
@@ -199,14 +270,18 @@ class Game:
             self.terms[self.d].change_pos(self.width, 3 * (self.height - 100) / 4 + 50)
             self.wave = False
 
-        self.terms[self.a].move(2)
+
+        self.terms[self.a].move(10)
         self.terms[self.a].draw(self.screen, self.width / 5, (self.height - 100) / 4)
-        self.terms[self.b].move(2)
+        self.terms[self.b].move(10)
         self.terms[self.b].draw(self.screen, self.width / 5, (self.height - 100) / 4)
-        self.terms[self.c].move(2)
+        self.terms[self.c].move(10)
         self.terms[self.c].draw(self.screen, self.width / 5, (self.height - 100) / 4)
-        self.terms[self.d].move(2)
+        self.terms[self.d].move(10)
         self.terms[self.d].draw(self.screen, self.width / 5, (self.height - 100) / 4)
+
+        # check if correct or not
+
 
         if self.terms[self.a].x < -250:
             self.wave = True
@@ -232,9 +307,10 @@ class Game:
 
             key = pygame.key.get_pressed()
 
-            space_pressed = key[pygame.K_SPACE] or key[pygame.K_w]
+            up = key[pygame.K_SPACE] or key[pygame.K_w]
+            down = key[pygame.K_s]
 
-            self.player.move(space_pressed, 50, self.height - 100)
+            self.player.move(up, down, self.height)
 
 
             for event in pygame.event.get():
